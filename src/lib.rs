07e132c39fc41b1d6defb92cb60806177cc801a7
Ivan <iab050811@gmail.com>
2026-05-08 17:46:02 +0200

    chore: bump version to 0.4.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
 pub mod request;
 pub mod response;
 pub mod notification;
+
+pub use request::XscpRequest;
+pub use request::OpCode;
+pub use request::RequestError;
+
+pub use response::XscpResponse;
+pub use response::ResponseError;
+
+pub use notification::XscpNotification;
+pub use notification::NotificationType;
+pub use notification::NotificationError;
